543c8a72e3b2ce0c94bbac74f915e8c40189e3a4
Quillbert <quillbert.quillbert@gmail.com>
2021-03-26 23:04:27 -0400

    added database

--- index.js
+++ index.js
@@ -4,10 +4,49 @@ var server = app.listen(process.env.PORT || 3000);
 app.use(express.static(__dirname + "/public"));
 var socket = require('socket.io');
 var io = socket(server);
+const admin = require('firebase-admin');
+const serviceAccount = require('./../hamchat-server-308821-01547b5a84a2.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
 console.log("socket started");
+
+async function addMessageToDataBase(data) {
+	const docRef = db.collection('messages');
+	var info = {
+		name: data.name,
+		message: data.message,
+		time: admin.firestore.FieldValue.serverTimestamp()
+	};
+	await docRef.add(info);
+	/*const lastThreeRes = await docRef.orderBy('time', 'desc').limit(50).get();
+	lastThreeRes.forEach((doc) => {
+  		console.log(doc.id, '=>', doc.data());
+	});*/
+}
+
+async function updateHistory(id) {
+	const docRef = db.collection('messages');
+	const info = await docRef.orderBy('time', 'desc').limit(50).get();
+	var messages = [];
+	info.forEach((doc) => {
+		messages.unshift(doc.data());
+	});
+	io.to(id).emit("history", messages);
+}
 
 io.on("connection", function(socket) {
 	socket.on("chat", function(data) {
-		io.emit("chat", data);
+		if(data.name != null && data.message != null) {
+			io.emit("chat", data);
+			addMessageToDataBase(data);
+		}
+	});
+	socket.on("history", function(data) {
+		updateHistory(socket.id);
 	});
 });
